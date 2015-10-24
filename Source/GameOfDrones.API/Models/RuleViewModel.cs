bfb0790af623f14d5c566cc2a815c78c176e237e
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2015-10-24 00:28:30 -0400

    create view model for rule creation

---
+++ Source/GameOfDrones.API/Models/RuleViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GameOfDrones.API.Models
+{
+    public class RuleViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string RuleDefinition { get; set; }
+    }
+}
