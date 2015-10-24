53692219609820f055b2c89ddbe5bfdf94327389
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2015-10-24 14:53:40 -0400

    implement basci endGameController logic and view

---
+++ Source/GameOfDrones.API/app/js/gameOfDrones/endGameController.js
@@ -0,0 +1,15 @@
+﻿angular.module('gameOfDrones').
+    controller('EndGameController', ['$scope', '$location', '$routeParams', 'matchDataService', function ($scope, $location, $routeParams, matchDataService) {
+
+        $scope.imageUrl = '/Content/Images/events.png';
+
+        $scope.winner = '';
+
+        var id = $routeParams.id;
+        matchDataService.getWinner(id)
+            .success(function(data) {
+                $scope.winner = data.winner.name;
+        });
+
+    }
+    ]);
