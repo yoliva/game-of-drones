835d684c579c3e3d623aec5ce577b3c639a8ccd1
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2015-10-24 22:08:50 -0400

    create action method in player api controller for retrieve the players stats

--- Source/GameOfDrones.API/Controllers/PlayersController.cs
+++ Source/GameOfDrones.API/Controllers/PlayersController.cs
@@ -1,5 +1,7 @@
-﻿using System.Web.Http;
+﻿using System.Linq;
+using System.Web.Http;
 using GameOfDrones.Domain.Entities;
+using GameOfDrones.Domain.Enums;
 using GameOfDrones.Domain.Repositories;
 
 namespace GameOfDrones.API.Controllers
@@ -24,11 +26,44 @@ namespace GameOfDrones.API.Controllers
         public IHttpActionResult GetPlayer(int playerId)
         {
             var player = _gameOfDronesRepository.GetPlayerById(playerId);
+
             if (player == null)
-            {
                 return NotFound();
-            }
+
             return Ok(player);
+        }
+
+        [Route("stats/{playerName}")]
+        public IHttpActionResult GetPlayerStats(string playerName)
+        {
+            var player = _gameOfDronesRepository.GetPlayerByName(playerName);
+
+            if (player == null)
+                return NotFound();
+
+            var validStats = player.PlayerStatses.Where(x => x.MatchResult != MatchResult.Invalid);
+            int wins = 0, loses = 0, wonRounds = 0, tiedRounds = 0, loseRounds = 0;
+            foreach (var playerStat in validStats)
+            {
+                if (playerStat.Match.Winner.Id == player.Id)
+                    wins++;
+                else
+                    loses++;
+
+                wonRounds += playerStat.WinnerRounds;
+                tiedRounds += playerStat.DrawRounds;
+                loseRounds += playerStat.LoserRounds;
+            }
+            return Ok(new
+            {
+                id = player.Id,
+                name=player.Name,
+                wins,
+                loses,
+                wonRounds,
+                tiedRounds,
+                loseRounds
+            });
         }
     }
 }
